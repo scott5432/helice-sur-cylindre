d348b8c578343ee5524927355de60c1ec9cfbe50
unknown <abdeljocelyn@gmail.com>
2023-06-25 20:10:47 +0200

    Modification des fichiers README.md et main.py

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# helice-sur-cylindre
+Ce code utilise la bibliothèque NumPy pour générer les coordonnées de l'hélice, et la bibliothèque Matplotlib avec l'option projection='3d' pour créer un graphique en trois dimensions. Voici une description étape par étape :
+
+1. Les paramètres du cylindre sont définis par : rayon_cylindre pour le rayon du cylindre et hauteur_cylindre pour sa hauteur.
+
+2. J'ai  défini  nombre_de_points pour spécifier le nombre de points sur l'hélice.
+
+3. Les coordonnées de l'hélice sont générées en utilisant np.linspace pour créer un nombre égal d'angles theta de 0 à 2pi, puis en utilisant np.linspace pour créer des coordonnées h sur la hauteur du cylindre, et np.linspace pour créer des coordonnées r sur le rayon du cylindre. Les coordonnées x et y sont calculées en fonction de r et theta.
+
+4. Une nouvelle figure est créée avec fig = plt.figure().
+
+5. Un sous-ensemble d'axes en 3D est ajouté à la figure avec ax = fig.add_subplot(111, projection='3d').
+
+6. Les points de l'hélice sont tracés en utilisant ax.scatter avec les coordonnées x, y et h, la couleur 'b' pour bleu et le marqueur '.' pour représenter chaque point.
+
+7. Les limites des axes sont configurées à l'aide des méthodes ax.set_xlim, ax.set_ylim et ax.set_zlim. Cela permet de définir les limites des valeurs affichées sur chaque axe.
+
+8. Enfin, la figure est affichée à l'aide de plt.show(), ce qui permet de visualiser le nuage de points représentant l'hélice sur le cylindre.
+
--- main.py
+++ main.py
@@ -1,29 +1,31 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import numpy as np                # Importation de la bibliothèque NumPy pour effectuer des calculs mathématiques
+import matplotlib.pyplot as plt  # Importation de la bibliothèque Matplotlib pour créer des graphiques
+from mpl_toolkits.mplot3d import Axes3D  # Importation de la classe Axes3D de la bibliothèque mpl_toolkits.mplot3d pour les graphiques en 3D
 
-# Paramètres de l'hélice
-rayon_cylindre = 1.0
-hauteur_cylindre = 2.0
-nombre_de_points = 100
 
-# Générer les coordonnées de l'hélice
-theta = np.linspace(0, 2 * np.pi, nombre_de_points)
-z = np.linspace(0, hauteur_cylindre, nombre_de_points)
-r = np.linspace(0, rayon_cylindre, nombre_de_points)
-x = r * np.cos(theta)
-y = r * np.sin(theta)
+# Paramétrage de l'hélice et du cylindre
+rayon_cylindre = 2.0
+hauteur_cylindre = 3.0
+nombre_de_points = 150
 
-# Créer la figure
+# les coordonnées de l'hélice
+theta = np.linspace(0, 2 * np.pi, nombre_de_points)  # Angles theta de 0 à 2pi
+h = np.linspace(0, hauteur_cylindre, nombre_de_points)  # Coordonnées h sur la hauteur du cylindre
+r = np.linspace(0, rayon_cylindre, nombre_de_points)  # Coordonnées r sur le rayon du cylindre
+x = r * np.cos(theta)  # Coordonnées x en fonction de r et theta
+y = r * np.sin(theta)  # Coordonnées y en fonction de r et theta
+
+# Céation de la figure
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+ax = fig.add_subplot(111, projection='3d') # création un sous-ensemble d'axes en 3D dans la figure
 
-# Tracer l'hélice sur le cylindre
-ax.plot(x, y, z, 'b.')
+# traçage de l'hélice sur le cylindre
+ax.scatter(x, y, h, c='b', marker='.')  # Traçage des points de l'hélice en bleu
 
-# Configurer les limites des axes
-ax.set_xlim(-rayon_cylindre, rayon_cylindre)
-ax.set_ylim(-rayon_cylindre, rayon_cylindre)
-ax.set_zlim(0, hauteur_cylindre)
+# Configuration des limites des axes
+ax.set_xlim(-rayon_cylindre, rayon_cylindre)  # Limite des valeurs x
+ax.set_ylim(-rayon_cylindre, rayon_cylindre)  # Limite des valeurs y
+ax.set_zlim(0, hauteur_cylindre)  # Limite des valeurs h
 
-# Afficher la figure
+# Affichage de la figure
 plt.show()
